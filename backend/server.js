f976a591913ef6321f60f2cea6aadb83531cfcf9
Tiago <tiagowu12@gmail.com>
2023-06-22 18:06:25 -0400

    Configure server setup and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const authRouter = require("./routers/authRouter");
+
+const app = express();
+app.use(express.json());
+
+app.use("/api", authRouter);
+
+const port = process.env.PORT || 5000;
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.set("strictQuery", true);
+(async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to MongoDB");
+
+    app.listen(port, () => {
+      console.log(`App is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error);
+  }
+})();
