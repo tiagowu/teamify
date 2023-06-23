a13ebf05c9d9d79f5e4ed01e8cb0a61ae37fc7ee
Tiago <tiagowu12@gmail.com>
2023-06-23 11:12:58 -0400

    Add error handling for signup functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,15 +7,19 @@ const authController = {
 
       const existingUser = await Users.findOne({ email });
       if (existingUser) {
-        return res.status(400).json({ message: "Email already exists" });
+        return res.status(409).json({ error: "Email already exists. Please choose a different email." });
       }
 
       const newUser = new Users({ fullName, email, password });
       await newUser.save();
 
-      res.status(201).json({ message: "Signup successful" });
+      return res.status(201).json({ message: "Registration successful" });
     } catch (err) {
-      res.status(500).json({ message: err.message });
+      if (err.name === "ValidationError") {
+        const validationErrors = Object.values(err.errors).map((error) => error.message);
+        return res.status(400).json({ errors: validationErrors });
+      }
+      return res.status(500).json({ error: "Internal server error. Please try again later." });
     }
   },
 };
