60a1f30e47a885243fef66f195ddb994ba2f300f
Tiago <tiagowu12@gmail.com>
2023-06-24 12:06:04 -0400

    Add refresh token route to auth router

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/authController");
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
+router.post("/refresh_token", authController.refreshToken);
 
 module.exports = router;
