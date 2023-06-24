b40c67be2cdddaad4298de3a33c422fe2d8d4c8d
Tiago <tiagowu12@gmail.com>
2023-06-23 21:31:51 -0400

    Add logout route to auth router

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -3,5 +3,6 @@ const authController = require("../controllers/authController");
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.post("/logout", authController.logout);
 
 module.exports = router;
