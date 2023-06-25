375f0a4d98787aed23a00e168ce3f5c5a62131b4
Tiago <tiagowu12@gmail.com>
2023-06-25 17:07:58 -0400

    Update team model with code field and generateUniqueCode method

--- backend/models/teamModel.js
+++ backend/models/teamModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const Schema = mongoose.Schema;
 
 const teamSchema = new mongoose.Schema(
   {
@@ -17,8 +17,33 @@ const teamSchema = new mongoose.Schema(
         ref: "Member",
       },
     ],
+    code: {
+      type: String,
+      unique: true,
+      required: true,
+    },
   },
   { timestamps: true }
 );
+
+teamSchema.statics.generateUniqueCode = async function (length) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let code = "";
+
+  while (true) {
+    code = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      code += characters.charAt(randomIndex);
+    }
+
+    const existingTeam = await this.findOne({ code });
+    if (!existingTeam) {
+      break;
+    }
+  }
+
+  return code;
+};
 
 module.exports = mongoose.model("Team", teamSchema);
