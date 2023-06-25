1d868960a9cfd7f644f56cc25de4ccafadd7d08f
Tiago <tiagowu12@gmail.com>
2023-06-25 17:11:17 -0400

    Implement teamController with team deletion functionality

--- backend/controllers/teamController.js
+++ backend/controllers/teamController.js
@@ -1,5 +1,6 @@
 const Member = require("../models/memberModel");
 const Team = require("../models/teamModel");
+const User = require("../models/userModel");
 
 const teamController = {
   createTeam: async (req, res) => {
@@ -20,6 +21,26 @@ const teamController = {
       await user.save();
 
       return res.status(201).json({ message: "Team created successfully.", team });
+    } catch (err) {
+      return res.status(500).json({ error: "Internal server error. Please try again later." });
+    }
+  },
+  deleteTeam: async (req, res) => {
+    try {
+      const teamId = req.params.teamId;
+
+      const deletedTeam = await Team.findOneAndDelete({ _id: teamId });
+      if (!deletedTeam) {
+        return res.status(404).json({ error: "Team not found or already deleted." });
+      }
+
+      const memberIds = deletedTeam.members;
+      const userIds = await Member.distinct("user", { _id: { $in: memberIds } });
+
+      await Member.deleteMany({ _id: { $in: memberIds } });
+      await User.updateMany({ _id: { $in: userIds } }, { $pull: { teams: teamId } });
+
+      return res.status(200).json({ message: "Team and associated members deleted successfully." });
     } catch (err) {
       return res.status(500).json({ error: "Internal server error. Please try again later." });
     }
