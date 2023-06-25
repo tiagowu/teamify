16ffaa9a415dd2b1973a78a29fe18fd6d6f8dc96
Tiago <tiagowu12@gmail.com>
2023-06-25 17:10:46 -0400

    Add team deletion route to team router

--- backend/routers/teamRouter.js
+++ backend/routers/teamRouter.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const teamController = require("../controllers/teamController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const teamMiddleware = require("../middlewares/teamMiddleware");
 
 router.post("/teams", authMiddleware.verifyToken, teamController.createTeam);
+router.delete("/teams/:teamId", authMiddleware.verifyToken, teamMiddleware.verifyManager, teamController.deleteTeam);
 
 module.exports = router;
