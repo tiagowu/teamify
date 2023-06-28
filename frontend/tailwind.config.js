4d509744e0596803ab0bf752221ba4178f194b15
Tiago <tiagowu12@gmail.com>
2023-06-28 12:28:31 -0400

    Update Tailwind CSS configuration file to include project files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,jsx,js,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
