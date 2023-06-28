aff69fd2a79f7de4a670371e4a0b23b0d44722a4
Tiago <tiagowu12@gmail.com>
2023-06-27 21:38:32 -0400

    Add join team with code route to user router

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const teamMiddleware = require("../middlewares/teamMiddleware");
 
 router.use(authMiddleware.verifyToken);
 
 router.post("/teams", userController.createTeam);
+router.post("/teams/join", teamMiddleware.verifyTeamCode, userController.joinTeamWithCode);
 
 module.exports = router;
