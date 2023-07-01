6994db94220658fbe5c055eda0603b5a530be2d7
Tiago <tiagowu12@gmail.com>
2023-06-30 22:01:01 -0400

    Add leave team route to user router

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -7,5 +7,6 @@ router.use(authMiddleware.verifyToken);
 
 router.post("/teams", userController.createTeam);
 router.post("/teams/join", teamMiddleware.verifyTeamCode, userController.joinTeamWithCode);
+router.delete("/teams/:teamId/leave", teamMiddleware.verifyTeamId, userController.leaveTeam);
 
 module.exports = router;
