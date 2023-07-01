1d9ea5807b0fcbf6b28494221c9b1d32a213efa8
Tiago <tiagowu12@gmail.com>
2023-06-30 22:06:44 -0400

    Add remove member route to team router

--- backend/routers/teamRouter.js
+++ backend/routers/teamRouter.js
@@ -5,8 +5,10 @@ const teamMiddleware = require("../middlewares/teamMiddleware");
 
 router.use(authMiddleware.verifyToken);
 router.param("teamId", teamMiddleware.verifyTeamId);
+router.param("memberId", teamMiddleware.verifyMemberId);
 
 router.post("/teams/:teamId/:userId/accept", teamMiddleware.checkPermission(["manager", "co-manager"]), teamController.acceptPendingRequest);
 router.delete("/teams/:teamId", teamMiddleware.checkPermission(["manager"]), teamController.deleteTeam);
+router.delete("/teams/members/:memberId", teamMiddleware.checkPermission(["manager", "co-manager"]), teamController.removeMember);
 
 module.exports = router;
