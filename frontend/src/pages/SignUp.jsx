bdb651cbc0f66f7c8ed95b258f6e691d9b7082b1
Tiago <tiagowu12@gmail.com>
2023-07-01 14:51:24 -0400

    Create SignUp page with SignUpForm component

---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import SignUpForm from "../components/SignUpForm";
+
+const Login = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default Login;
