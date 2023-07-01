3bc2eb5691e32a0411a6a0287651101a81af26c2
Tiago <tiagowu12@gmail.com>
2023-07-01 14:17:11 -0400

    Create Login page with LoginForm component

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import LoginForm from "../components/LoginForm";
+
+const Login = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
