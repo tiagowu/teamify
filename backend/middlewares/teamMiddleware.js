416df206211e6f3d79ede1026dbf9c7bf9dcd8a8
Tiago <tiagowu12@gmail.com>
2023-06-30 21:56:15 -0400

    Change verifyManager to checkPermission in teamMiddleware  for role-based authorization

--- backend/middlewares/teamMiddleware.js
+++ backend/middlewares/teamMiddleware.js
@@ -1,34 +1,22 @@
-const mongoose = require("mongoose");
-const Team = require("../models/teamModel.js");
 const Member = require("../models/memberModel.js");
-const ObjectId = mongoose.Types.ObjectId;
 
 const teamMiddleware = {
-  verifyManager: async (req, res, next) => {
-    try {
-      const teamId = req.params.teamId;
-      const userId = req.user._id;
+  checkPermission: (requiredRoles) => {
+    return async (req, res, next) => {
+      try {
+        const userId = req.user._id;
+        const teamId = req.team._id;
 
-      if (!ObjectId.isValid(teamId)) {
-        return res.status(400).json({ error: "Team not found." });
+        const member = await Member.findOne({ user: userId, team: teamId, role: { $in: requiredRoles } });
+        if (!member) {
+          return res.status(403).json({ error: "You are unauthorized to perform this action." });
+        }
+
+        next();
+      } catch (err) {
+        return res.status(500).json({ error: "Internal server error. Please try again later." });
       }
-
-      const team = await Team.findById(teamId);
-      if (!team) {
-        return res.status(404).json({ error: "Team not found." });
-      }
-
-      const member = await Member.findOne({ user: userId, team: teamId, role: "manager" });
-      if (!member) {
-        return res.status(403).json({ error: "You are unauthorized to perform this action" });
-      }
-
-      req.team = team;
-      req.member = member;
-      next();
-    } catch (err) {
-      return res.status(500).json({ error: "Internal server error. Please try again later." });
-    }
+    };
   },
 };
 
