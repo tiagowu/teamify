bef2ddc107b7cf7fd082d7ed00c893037e078f96
Tiago <tiagowu12@gmail.com>
2023-06-30 21:57:42 -0400

    Implement verifyTeamId functionality in teamMiddleware

--- backend/middlewares/teamMiddleware.js
+++ backend/middlewares/teamMiddleware.js
@@ -1,6 +1,22 @@
+const Team = require("../models/teamModel.js");
 const Member = require("../models/memberModel.js");
 
 const teamMiddleware = {
+  verifyTeamId: async (req, res, next) => {
+    try {
+      const teamId = req.params.teamId;
+
+      const team = await Team.findById(teamId);
+      if (!team) {
+        return res.status(404).json({ error: "Team not found." });
+      }
+
+      req.team = team;
+      next();
+    } catch (err) {
+      return res.status(500).json({ error: "Internal server error. Please try again later." });
+    }
+  },
   checkPermission: (requiredRoles) => {
     return async (req, res, next) => {
       try {
