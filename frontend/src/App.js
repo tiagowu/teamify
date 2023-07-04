eb2ad512fcdac5456dbc5fa7c0fcb76195839c4a
Tiago <tiagowu12@gmail.com>
2023-07-03 22:26:47 -0400

    Update App component to use react-router-dom for routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
-import Login from "./pages/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <>
-      <Login />
-      <SignUp />
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
