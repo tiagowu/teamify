e9e3c5f78721b6d36d94b68cbffc8d01e02818aa
Tiago <tiagowu12@gmail.com>
2023-07-04 22:04:10 -0400

    Implement Add AuthContext and AuthProvider components to manage auth and persist states

---
+++ frontend/src/context/AuthProvider.js
@@ -0,0 +1,12 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({});
+  const [persist, setPersist] = useState(true);
+
+  return <AuthContext.Provider value={{ auth, setAuth, persist, setPersist }}>{children}</AuthContext.Provider>;
+};
+
+export default AuthContext;
