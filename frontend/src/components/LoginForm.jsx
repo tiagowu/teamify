3424b01d82c11f1eca1e5bedbf137376f75ae42d
Tiago <tiagowu12@gmail.com>
2023-07-04 22:08:03 -0400

    Update handleSubmit function in LoginForm component

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,8 +1,24 @@
+import useAuth from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+
+import { postData } from "../api/axios";
 import Form from "./Form";
 
 const LoginForm = () => {
-  const handleSubmit = (userData) => {
+  const { setAuth } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (userData) => {
     console.log("Login:", userData);
+    try {
+      const res = await postData("login", userData);
+      const accessToken = res?.data?.accessToken;
+      const user = res?.data?.user;
+      setAuth({ user, accessToken });
+    } catch (err) {
+      console.log(err);
+    }
+    navigate("/dashboard", { replace: true });
   };
 
   const loginFields = [
