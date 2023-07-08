f0a3e2104554d9a02ee8725cbdc0ef1313ef0e75
Tiago <tiagowu12@gmail.com>
2023-07-07 22:20:00 -0400

    Implement getData function using axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+export const getData = async (url, token) => {
+  const res = await axios.get(`/api/${url}`, {
+    headers: { Authorization: token },
+  });
+  return res;
+};
+
 export const postData = async (url, post, token) => {
   const res = await axios.post(`/api/${url}`, post, {
     headers: { Authorization: token },
