359df962151b9a9d0306a1cb54029ec62f4fe589
Tiago <tiagowu12@gmail.com>
2023-07-07 22:18:11 -0400

    Implement loading context and hook in LoginForm component

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,24 +1,31 @@
-import useAuth from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 
+import useAuth from "../hooks/useAuth";
+import useLoading from "../hooks/useLoading";
 import { postData } from "../api/axios";
+import Loading from "../pages/Loading";
 import Form from "./Form";
 
 const LoginForm = () => {
+  const { isLoading, setIsLoading } = useLoading();
   const { setAuth } = useAuth();
   const navigate = useNavigate();
 
   const handleSubmit = async (userData) => {
-    console.log("Login:", userData);
     try {
+      setIsLoading(true);
       const res = await postData("login", userData);
       const accessToken = res?.data?.accessToken;
       const user = res?.data?.user;
       setAuth({ user, accessToken });
     } catch (err) {
       console.log(err);
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false);
+        navigate("/dashboard", { replace: true });
+      }, 200);
     }
-    navigate("/dashboard", { replace: true });
   };
 
   const loginFields = [
@@ -26,7 +33,15 @@ const LoginForm = () => {
     { id: "login-password", type: "password", label: "Password", name: "password" },
   ];
 
-  return <Form fields={loginFields} onSubmit={handleSubmit} buttonText="Login" text="Don't have an account?" link="/signup" linkText="Sign Up" />;
+  return (
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Form fields={loginFields} onSubmit={handleSubmit} buttonText="Login" text="Don't have an account?" link="/signup" linkText="Sign Up" />
+      )}
+    </>
+  );
 };
 
 export default LoginForm;
