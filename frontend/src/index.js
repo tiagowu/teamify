2b0f64e5cbb44093de2ad4f8681c64fc25478e82
Tiago <tiagowu12@gmail.com>
2023-07-08 22:16:47 -0400

    Update index.js to render the App component within LoadingProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import { LoadingProvider } from "./context/LoadingContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthProvider>
-    <App />
+    <LoadingProvider>
+      <App />
+    </LoadingProvider>
   </AuthProvider>
 );
