a891f8c38af3e7118c1599cb145081fb953a3409
Tiago <tiagowu12@gmail.com>
2023-07-08 22:10:10 -0400

    Add PublicRoute component as a wrapper for login and signup routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,17 @@ import Login from "./pages/Login";
 import Missing from "./pages/Missing";
 import SignUp from "./pages/SignUp";
 
+import PublicRoute from "./components/PublicRoute";
+
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route exact path="signup" element={<SignUp />} />
+        <Route element={<PublicRoute />}>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="signup" element={<SignUp />} />
+        </Route>
+
         <Route exact path="/dashboard" element={<Dashboard />} />
         <Route exact path="*" element={<Missing />} />
       </Routes>
