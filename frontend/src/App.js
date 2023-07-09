483eebea822846f6babab023dbe538628634b315
Tiago <tiagowu12@gmail.com>
2023-07-08 22:08:27 -0400

    Update routes and rendered pages in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
-import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import Missing from "./pages/Missing";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signup" element={<SignUp />} />
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="signup" element={<SignUp />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="*" element={<Missing />} />
       </Routes>
     </Router>
   );
