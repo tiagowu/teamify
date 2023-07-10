4687c3b7bb9964a389de89c8d5c48cdd4cbf4f3d
Tiago <tiagowu12@gmail.com>
2023-07-09 22:13:01 -0400

    Refactor Form component

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,34 +1,18 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import { ReactComponent as Logo } from "../assets/Logo.svg";
 import InputField from "./InputField";
 
 import { Link } from "react-router-dom";
 
-const Form = ({ fields, onSubmit, buttonText, text, link, linkText }) => {
-  const [userData, setUserData] = useState(Object.fromEntries(fields.map((field) => [field.name, ""])));
+const Form = ({ buttonText, data, disabled, fields, handleChange, handleSubmit, link, linkText, text }) => {
   const userRef = useRef();
 
   useEffect(() => {
     userRef.current.focus();
   }, []);
 
-  const isEmailValid = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit(userData);
-  };
-
-  const handleDataChange = (e) => {
-    const { name, value } = e.target;
-    setUserData({ ...userData, [name]: value });
-  };
-
   return (
-    <form className="flex flex-col justify-center max-w-lg w-full bg-white shadow-md rounded p-8 m-8" onSubmit={handleSubmit} noValidate>
+    <form className="flex flex-col justify-center max-w-lg w-full bg-white shadow-md rounded p-4" onSubmit={handleSubmit} noValidate>
       <Logo className="h-24 text-blue-400" />
       {fields.map((field, index) => (
         <InputField
@@ -37,15 +21,17 @@ const Form = ({ fields, onSubmit, buttonText, text, link, linkText }) => {
           type={field.type}
           label={field.label}
           name={field.name}
-          value={userData[field.name]}
-          handleChange={handleDataChange}
+          value={data[field.name]}
+          handleChange={handleChange}
+          handleKeyDown={field.onKeyDown ? field.onKeyDown : null}
+          maxLength={field.maxLength ? field.maxLength : null}
           ref={index === 0 ? userRef : null}
         />
       ))}
       <button
         className="mx-auto w-20 border rounded text-white bg-blue-500 p-2 mt-2 disabled:bg-blue-400 hover:bg-blue-600"
         type="submit"
-        disabled={!isEmailValid(userData.email) || fields.some((field) => !userData[field.name]) || userData.password.length < 6}
+        disabled={fields.some((field) => !data[field.name]) || disabled}
       >
         {buttonText}
       </button>
