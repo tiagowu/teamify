141f1bf3f264c10f0415e778144f100b0c2bf086
Tiago <tiagowu12@gmail.com>
2023-07-09 22:10:22 -0400

    Fix authorization header in API requests

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,35 +2,35 @@ import axios from "axios";
 
 export const deleteData = async (url, post, token) => {
   const res = await axios.delete(`/url/${url}`, post, {
-    headers: { Authorization: token },
+    headers: { Authorization: `Bearer ${token}` },
   });
   return res;
 };
 
 export const getData = async (url, token) => {
   const res = await axios.get(`/api/${url}`, {
-    headers: { Authorization: token },
+    headers: { Authorization: `Bearer ${token}` },
   });
   return res;
 };
 
 export const patchData = async (url, post, token) => {
   const res = await axios.patch(`/url/${url}`, post, {
-    headers: { Authorization: token },
+    headers: { Authorization: `Bearer ${token}` },
   });
   return res;
 };
 
 export const postData = async (url, post, token) => {
   const res = await axios.post(`/api/${url}`, post, {
-    headers: { Authorization: token },
+    headers: { Authorization: `Bearer ${token}` },
   });
   return res;
 };
 
 export const putData = async (url, post, token) => {
   const res = await axios.put(`/url/${url}`, post, {
-    headers: { Authorization: token },
+    headers: { Authorization: `Bearer ${token}` },
   });
   return res;
 };
