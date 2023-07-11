0fa1b8ac2eb62bac46f6faa0848d4133cfd1ff05
Tiago <tiagowu12@gmail.com>
2023-07-11 17:15:37 -0400

    Update initial state of isLoading in LoadingContext to false

--- frontend/src/context/LoadingContext.js
+++ frontend/src/context/LoadingContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 const LoadingContext = createContext({});
 
 export const LoadingProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   return <LoadingContext.Provider value={{ isLoading, setIsLoading }}>{children}</LoadingContext.Provider>;
 };
