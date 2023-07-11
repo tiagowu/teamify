bffd39717f92e4ebd562f341421f52ce2cf35809
Tiago <tiagowu12@gmail.com>
2023-07-10 21:58:40 -0400

    Add get teams route to team router

--- backend/routers/teamRouter.js
+++ backend/routers/teamRouter.js
@@ -7,8 +7,9 @@ router.use(authMiddleware.verifyToken);
 router.param("teamId", teamMiddleware.verifyTeamId);
 router.param("memberId", teamMiddleware.verifyMemberId);
 
-router.post("/teams/:teamId/:userId/accept", teamMiddleware.checkPermission(["manager", "co-manager"]), teamController.acceptPendingRequest);
-router.delete("/teams/:teamId", teamMiddleware.checkPermission(["manager"]), teamController.deleteTeam);
-router.delete("/teams/members/:memberId", teamMiddleware.checkPermission(["manager", "co-manager"]), teamController.removeMember);
+router.get("/teams", teamController.getUserTeams);
+router.post("/teams/:teamId/:userId/accept", teamMiddleware.checkPermission(["Manager", "Co-Manager"]), teamController.acceptPendingRequest);
+router.delete("/teams/:teamId", teamMiddleware.checkPermission(["Manager"]), teamController.deleteTeam);
+router.delete("/teams/members/:memberId", teamMiddleware.checkPermission(["Manager", "Co-Manager"]), teamController.removeMember);
 
 module.exports = router;
