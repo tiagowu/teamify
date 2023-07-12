9acbb701ffa6f6618fa4d7bfa5adaa96cc286238
Tiago <tiagowu12@gmail.com>
2023-07-12 11:10:15 -0400

    Refactor SignUpForm component to use local state

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import useAuth from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 
@@ -5,20 +6,9 @@ import { postData } from "../api/axios";
 import Form from "./Form";
 
 const SignUpForm = () => {
+  const [data, setData] = useState({ fullName: "", email: "", password: "" });
   const { setAuth } = useAuth();
   const navigate = useNavigate();
-
-  const handleSubmit = async (userData) => {
-    try {
-      const res = await postData("signup", userData);
-      const accessToken = res?.data?.accessToken;
-      const user = res?.data?.user;
-      setAuth({ user, accessToken });
-    } catch (err) {
-      console.log(err);
-    }
-    navigate("/dashboard", { replace: true });
-  };
 
   const signupFields = [
     { id: "signup-name", type: "text", label: "Full Name", name: "fullName" },
@@ -26,7 +16,46 @@ const SignUpForm = () => {
     { id: "signup-password", type: "password", label: "Password", name: "password" },
   ];
 
-  return <Form fields={signupFields} onSubmit={handleSubmit} buttonText="Sign Up" text="Already have an account?" link="/" linkText="Login" />;
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await postData("signup", data);
+      const accessToken = res?.data?.accessToken;
+      const user = res?.data?.user;
+      setAuth({ user, accessToken });
+      setTimeout(() => {
+        navigate("/dashboard", { replace: true });
+      }, 200);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDataChange = (e) => {
+    const { name, value } = e.target;
+    setData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const isEmailValid = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const disabled = !isEmailValid(data.email) || data.password.length < 6;
+
+  return (
+    <Form
+      buttonText="Sign Up"
+      fields={signupFields}
+      data={data}
+      disabled={disabled}
+      handleChange={handleDataChange}
+      handleSubmit={handleSignUp}
+      link="/"
+      linkText="Login"
+      text="Already have an account?"
+    />
+  );
 };
 
 export default SignUpForm;
