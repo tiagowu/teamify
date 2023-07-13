c23d8feaad99e67f2601e59e927e4c701d758044
Tiago <tiagowu12@gmail.com>
2023-07-13 17:32:31 -0400

    Add useMessage hook to Form component for displaying success or error messages

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,11 +1,17 @@
 import { useRef, useEffect } from "react";
 import { ReactComponent as Logo } from "../assets/Logo.svg";
 import InputField from "./InputField";
+import useMessage from "../hooks/useMessage";
 
 import { Link } from "react-router-dom";
 
 const Form = ({ buttonText, data, disabled, fields, handleChange, handleSubmit, link, linkText, text }) => {
+  const { message } = useMessage();
   const userRef = useRef();
+
+  const getMessageColor = () => {
+    return message && message.type === "success" ? "text-green-500" : "text-red-500";
+  };
 
   useEffect(() => {
     userRef.current.focus();
@@ -28,6 +34,7 @@ const Form = ({ buttonText, data, disabled, fields, handleChange, handleSubmit, 
           ref={index === 0 ? userRef : null}
         />
       ))}
+      {message && <p className={`text-center py-2 ${getMessageColor()}`}>{message.content}</p>}
       <button
         className="mx-auto w-20 border rounded text-white bg-blue-500 p-2 mt-2 disabled:bg-blue-400 hover:bg-blue-600"
         type="submit"
@@ -35,7 +42,7 @@ const Form = ({ buttonText, data, disabled, fields, handleChange, handleSubmit, 
       >
         {buttonText}
       </button>
-      <p className="mx-auto p-2">
+      <p className="text-center p-2">
         {text}{" "}
         <Link to={link} className="text-blue-600">
           {linkText}
