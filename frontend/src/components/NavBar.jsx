2a8c758b022ade2d21eac38ffb751eb14289db8c
Tiago <tiagowu12@gmail.com>
2023-07-14 22:05:50 -0400

    Add user's full name to NavBar component and refactor

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,57 +1,53 @@
 import { useNavigate, Link } from "react-router-dom";
 import { MdDashboard, MdLogout } from "react-icons/md";
 
-import { postData } from "../api/axios";
 import useAuth from "../hooks/useAuth";
 import useLoading from "../hooks/useLoading";
 
+import { logout } from "../api/auth";
 import { ReactComponent as Logo } from "../assets/Logo.svg";
-import Loading from "../pages/Loading";
 
 const NavBar = () => {
-  const { isLoading, setIsLoading } = useLoading();
-  const { setAuth } = useAuth();
+  const { setIsLoading } = useLoading();
+  const { auth, setAuth } = useAuth();
   const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
       setIsLoading(true);
-      await postData("logout");
+      await logout();
       setAuth({});
+      navigate("/", { replace: true });
+      setIsLoading(false);
     } catch (err) {
       console.log(err);
-    } finally {
-      setTimeout(() => {
-        setIsLoading(false);
-        navigate("/", { replace: true });
-      }, 200);
+      setIsLoading(false);
     }
   };
 
   return (
-    <>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <nav className="absolute w-full min-w-[320px] flex items-center justify-between px-4 h-16 bg-gray-900 text-white">
-          <Link to="/dashboard">
-            <Logo className="h-full w-32 text-blue-400" />
+    <nav className="absolute w-full min-w-[320px] flex items-center justify-between px-4 h-16 bg-gray-900 text-white">
+      <Link to="/dashboard">
+        <Logo className="h-full w-32 text-blue-400" />
+      </Link>
+      <ul className="flex items-center space-x-3">
+        <li className="hidden sm:block">
+          <div className="truncate max-w-[320px] bg-blue-400 rounded-full px-3 py-2">
+            {auth.user && <span className="text-md text-right">{auth.user.fullName}</span>}
+          </div>
+        </li>
+        <li>
+          <Link to="/dashboard" className="text-4xl">
+            <MdDashboard />
           </Link>
-          <ul className="flex items-center space-x-4">
-            <li>
-              <Link to="/dashboard" className="text-4xl">
-                <MdDashboard />
-              </Link>
-            </li>
-            <li>
-              <button onClick={handleLogout} className="flex items-center text-4xl">
-                <MdLogout />
-              </button>
-            </li>
-          </ul>
-        </nav>
-      )}
-    </>
+        </li>
+        <li>
+          <button onClick={handleLogout} className="flex items-center text-4xl">
+            <MdLogout />
+          </button>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
