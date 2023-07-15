ae060ba8b375ec68ec48deb1f82395a8a1a2d68a
Tiago <tiagowu12@gmail.com>
2023-07-14 22:07:14 -0400

    Update index.js to render the App component within MessageProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { LoadingProvider } from "./context/LoadingContext";
+import { MessageProvider } from "./context/MessageContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthProvider>
     <LoadingProvider>
-      <App />
+      <MessageProvider>
+        <App />
+      </MessageProvider>
     </LoadingProvider>
   </AuthProvider>
 );
