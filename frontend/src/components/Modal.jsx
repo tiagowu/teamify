3ad8708ef2dddf42d3169d9222bd7197702b7d29
Tiago <tiagowu12@gmail.com>
2023-07-18 14:47:58 -0400

    Add scrollbar styling to Modal component

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -12,7 +12,10 @@ const Modal = ({ children, closeModal }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center" onClick={handleClose}>
-      <div ref={modalRef} className="bg-white w-3/4 lg:w-1/2 rounded z-50">
+      <div
+        ref={modalRef}
+        className="overflow-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-rounded scrollbar-thumb-slate-300 scrollbar-track-transparent scrollbar-track-rounded max-h-[90%] bg-white w-3/4 lg:w-1/2 rounded z-50"
+      >
         <div className="flex flex-col items-center p-4 relative">
           <button className="absolute top-0 right-0 p-4 text-xl" onClick={closeModal}>
             <MdClose />
