6126c1cc23bf404682414c00a23f2a92ad08d24c
Tiago <tiagowu12@gmail.com>
2023-07-17 21:49:01 -0400

    Refactor TeamList component using DataList component for improved modularity

--- frontend/src/components/TeamList.jsx
+++ frontend/src/components/TeamList.jsx
@@ -1,39 +1,31 @@
-import { useEffect, useState } from "react";
-import { getData } from "../api/axios";
-import useAuth from "../hooks/useAuth";
-import useLoading from "../hooks/useLoading";
+import { MdGroupAdd, MdAdd } from "react-icons/md";
 
-import TeamHeader from "./TeamHeader";
+import CreateTeamForm from "./CreateTeamForm";
+import DataList from "./DataList";
+import JoinTeamForm from "./JoinTeamForm";
 import TeamItem from "./TeamItem";
 
-const TeamList = () => {
-  const [teams, setTeams] = useState([]);
-  const { auth } = useAuth();
-  const { setIsLoading } = useLoading();
+const TeamList = ({ teams }) => {
+  const teamButtons = [
+    {
+      key: "join-team",
+      icon: <MdGroupAdd />,
+      modal: {
+        title: "Join Team",
+        component: <JoinTeamForm />,
+      },
+    },
+    {
+      key: "create-team",
+      icon: <MdAdd />,
+      modal: {
+        title: "Create New Team",
+        component: <CreateTeamForm />,
+      },
+    },
+  ];
 
-  useEffect(() => {
-    const fetchTeams = async () => {
-      try {
-        const res = await getData("teams", auth.accessToken);
-        setTeams(res.data.teams);
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    if (auth.user?.teams?.length > 0) fetchTeams();
-  }, [auth.accessToken, auth.user.teams, setIsLoading]);
-
-  return (
-    <div className="rounded h-full bg-gray-200 overflow-y-auto overscroll-none scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-rounded scrollbar-thumb-slate-300 scrollbar-track-transparent scrollbar-track-rounded ">
-      <TeamHeader />
-      {teams.map((team) => (
-        <TeamItem key={team.id} team={team} />
-      ))}
-    </div>
-  );
+  return <DataList buttons={teamButtons} item={TeamItem} list={teams} title="TEAMS" vertical />;
 };
 
 export default TeamList;
