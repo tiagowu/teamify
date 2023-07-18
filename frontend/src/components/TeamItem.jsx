eab72453e91a604b121e4c1816871cbd883b836f
Tiago <tiagowu12@gmail.com>
2023-07-17 21:53:35 -0400

    Update TeamItem component to include a link to team page

--- frontend/src/components/TeamItem.jsx
+++ frontend/src/components/TeamItem.jsx
@@ -1,11 +1,15 @@
-const TeamItem = ({ team }) => {
+import { Link } from "react-router-dom";
+
+const TeamItem = ({ item }) => {
   return (
     <div className="w-full bg-inherit px-2 py-1">
-      <div className="w-full bg-slate-300 rounded-lg p-3">
-        <span className="whitespace-normal break-words text-lg">{team.name}</span>
-        <span className="whitespace-normal break-words text-gray-600 text-sm"> {team.role}</span>
-        <p className="text-sm whitespace-normal break-words">{team.description}</p>
-      </div>
+      <Link to={`/team/${item.id}`}>
+        <div className="w-full bg-slate-300 rounded-lg p-3">
+          <span className="whitespace-normal break-words text-lg">{item.name}</span>
+          <span className="whitespace-normal break-words text-gray-600 text-sm"> {item.role}</span>
+          <p className="text-sm whitespace-normal break-words">{item.description}</p>
+        </div>
+      </Link>
     </div>
   );
 };
