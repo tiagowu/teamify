bfa970b15fd3e038b3cbd4166b4267c873e8236c
Tiago <tiagowu12@gmail.com>
2023-07-19 22:15:26 -0400

    Update DataList component styles

--- frontend/src/components/DataList.jsx
+++ frontend/src/components/DataList.jsx
@@ -5,7 +5,7 @@ const DataList = ({ buttons, item: Item, list, title, vertical }) => {
     <div
       className={`flex ${
         vertical && "flex-col"
-      } rounded h-full w-full overflow-auto overscroll-none scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-rounded scrollbar-thumb-slate-300 scrollbar-track-transparent scrollbar-track-rounded`}
+      } rounded h-full w-full gap-1 overflow-auto overscroll-none scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-rounded scrollbar-thumb-slate-300 scrollbar-track-transparent scrollbar-track-rounded`}
     >
       <DataHeader title={title} buttons={buttons} vertical={vertical} />
       {list.map((item) => (
