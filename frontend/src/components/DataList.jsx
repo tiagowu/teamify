f9f9f8b17ac8a4f038b86a79da657ba843e0c665
Tiago <tiagowu12@gmail.com>
2023-07-20 22:00:27 -0400

    Update DataList to display message when list is empty

--- frontend/src/components/DataList.jsx
+++ frontend/src/components/DataList.jsx
@@ -8,9 +8,11 @@ const DataList = ({ buttons, item: Item, list, title, vertical }) => {
       } rounded h-full w-full gap-1 overflow-auto overscroll-none scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-rounded scrollbar-thumb-slate-300 scrollbar-track-transparent scrollbar-track-rounded`}
     >
       <DataHeader title={title} buttons={buttons} vertical={vertical} />
-      {list.map((item) => (
-        <Item key={item._id} item={item} />
-      ))}
+      {list.length > 0 ? (
+        list.map((item) => <Item key={item._id} item={item} />)
+      ) : (
+        <div className="w-full h-full flex justify-center items-center font-bold text-gray-400/50 text-center">NO {title} TO DISPLAY</div>
+      )}
     </div>
   );
 };
