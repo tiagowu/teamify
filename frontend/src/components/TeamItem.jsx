3fdf7a39a8e84f535b16a8be24235a807506f376
Tiago <tiagowu12@gmail.com>
2023-07-20 22:07:23 -0400

    Update TeamItem component styles

--- frontend/src/components/TeamItem.jsx
+++ frontend/src/components/TeamItem.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const TeamItem = ({ item }) => {
   return (
-    <div className="w-full bg-inherit px-2 py-1">
+    <div className="w-full bg-inherit px-2">
       <Link to={`/team/${item._id}`}>
         <div className="w-full bg-slate-300 rounded-lg p-3">
           <span className="whitespace-normal break-words text-lg">{item.name}</span>
