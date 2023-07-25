220953314b48c6b25be4823424424ea9574a9731
Tiago <tiagowu12@gmail.com>
2023-07-24 21:45:23 -0400

    Add leave team route to teamRouter

--- backend/routers/teamRouter.js
+++ backend/routers/teamRouter.js
@@ -10,6 +10,7 @@ router.param("memberId", teamMiddleware.verifyMemberId);
 router.get("/teams", teamController.getUserTeams);
 router.get("/teams/:teamId", teamController.getTeamById);
 router.delete("/teams/:teamId", teamMiddleware.checkPermission(["Manager"]), teamController.deleteTeam);
+router.delete("/teams/:teamId/leave", teamController.leaveTeam);
 
 router.post("/teams/:teamId/:userId/accept", teamMiddleware.checkPermission(["Manager", "Co-Manager"]), teamController.acceptPendingRequest);
 router.delete("/teams/:teamId/members/:memberId", teamMiddleware.checkPermission(["Manager"]), teamController.removeMember);
