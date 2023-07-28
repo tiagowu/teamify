67d4c582aa3071ad958588d4380f38b51f90bb0c
Tiago <tiagowu12@gmail.com>
2023-07-27 21:26:13 -0400

    Implement team API calls for deleting and leaving team

--- frontend/src/api/team.js
+++ frontend/src/api/team.js
@@ -9,6 +9,24 @@ export const getTeamById = async (teamId, token) => {
   }
 };
 
+export const deleteTeam = async (teamId, token) => {
+  try {
+    const response = await deleteData(`teams/${teamId}`, token);
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const leaveTeam = async (teamId, token) => {
+  try {
+    const response = await deleteData(`teams/${teamId}/leave`, token);
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
 export const createProject = async (data, teamId, token) => {
   try {
     console.log(teamId);
