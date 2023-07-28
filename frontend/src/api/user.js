9efb2466d86a842cada645e3d72d98a367b615cf
Tiago <tiagowu12@gmail.com>
2023-07-27 21:32:35 -0400

    Update API response handling in createTeam and joinTeam functions

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -3,7 +3,7 @@ import { postData } from "./axios";
 export const createTeam = async (data, token) => {
   try {
     const response = await postData("teams", data, token);
-    return response;
+    return response.data;
   } catch (err) {
     throw err;
   }
@@ -12,7 +12,7 @@ export const createTeam = async (data, token) => {
 export const joinTeam = async (data, token) => {
   try {
     const response = await postData("teams/join", data, token);
-    return response;
+    return response.data;
   } catch (err) {
     throw err;
   }
