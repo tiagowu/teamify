bfd24b04e6b1deb3e117463ad02a698f437f0b68
Tiago <tiagowu12@gmail.com>
2023-07-27 21:28:16 -0400

    Implement team API calls for updating member and project

--- frontend/src/api/team.js
+++ frontend/src/api/team.js
@@ -45,11 +45,29 @@ export const declineRequest = async (teamId, userId, token) => {
   }
 };
 
+export const updateMember = async (data, teamId, memberId, token) => {
+  try {
+    const response = await putData(`teams/${teamId}/members/${memberId}`, data, token);
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
 export const createProject = async (data, teamId, token) => {
   try {
     console.log(teamId);
     const response = await postData(`teams/${teamId}/projects`, data, token);
     return response;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const updateProject = async (data, teamId, projectId, token) => {
+  try {
+    const response = await putData(`teams/${teamId}/projects/${projectId}`, data, token);
+    return response.data;
   } catch (err) {
     throw err;
   }
