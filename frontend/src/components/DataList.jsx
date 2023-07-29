e3d6bad7a87ee363d8abc93802721e39e3a315ab
Tiago <tiagowu12@gmail.com>
2023-07-28 21:30:42 -0400

    Add role prop to DataList component

--- frontend/src/components/DataList.jsx
+++ frontend/src/components/DataList.jsx
@@ -1,15 +1,15 @@
 import DataHeader from "./DataHeader";
 
-const DataList = ({ buttons, item: Item, list, title, vertical }) => {
+const DataList = ({ buttons, item: Item, list, role, title, vertical }) => {
   return (
     <div
       className={`flex ${
         vertical && "flex-col"
-      } rounded h-full w-full gap-1 overflow-auto overscroll-none scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-rounded scrollbar-thumb-slate-300 scrollbar-track-transparent scrollbar-track-rounded`}
+      } rounded h-full w-full gap-1 overflow-auto overscroll-none scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-slate-300 scrollbar-track-transparent scrollbar-track-rounded`}
     >
       <DataHeader title={title} buttons={buttons} vertical={vertical} />
-      {list.length > 0 ? (
-        list.map((item) => <Item key={item._id} item={item} />)
+      {list && list.length > 0 ? (
+        list.map((item) => <Item key={item._id} item={item} role={role} />)
       ) : (
         <div className="w-full h-full flex justify-center items-center font-bold text-gray-400/50 text-center">NO {title} TO DISPLAY</div>
       )}
