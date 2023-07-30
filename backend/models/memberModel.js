62bb3e198aff6eb34594e87e97c6223b30566734
Tiago <tiagowu12@gmail.com>
2023-07-29 21:37:59 -0400

    Update Member model to include timestamps

--- backend/models/memberModel.js
+++ backend/models/memberModel.js
@@ -1,28 +1,31 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const memberSchema = new Schema({
-  team: {
-    type: Schema.Types.ObjectId,
-    ref: "Team",
-    required: true,
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  role: {
-    type: String,
-    enum: ["Manager", "Co-Manager", "Member"],
-    default: "Member",
-  },
-  projects: [
-    {
+const memberSchema = new Schema(
+  {
+    team: {
       type: Schema.Types.ObjectId,
-      ref: "Project",
+      ref: "Team",
+      required: true,
     },
-  ],
-});
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    role: {
+      type: String,
+      enum: ["Manager", "Co-Manager", "Member"],
+      default: "Member",
+    },
+    projects: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 memberSchema.statics.createMember = async function (userId, teamId, role) {
   const member = new this({ user: userId, team: teamId, role });
