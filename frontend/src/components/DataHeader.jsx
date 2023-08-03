b0ce22e1a9332a0a1a3bb4be2fb734fdecaaef02
Tiago <tiagowu12@gmail.com>
2023-08-03 16:02:32 -0400

    Adjust styles and fix button rendering in DataHeader component

--- frontend/src/components/DataHeader.jsx
+++ frontend/src/components/DataHeader.jsx
@@ -15,24 +15,23 @@ const DataHeader = ({ buttons, title, vertical }) => {
 
   return (
     <>
-      <div className={`flex justify-center sticky top-0 z-30 bg-blue-400 p-2 rounded ${!vertical && "flex-col left-0"}`}>
+      <div className={`flex justify-center sticky top-0 z-20 bg-blue-400 p-2 rounded ${!vertical && "flex-col left-0"}`}>
         <p className={`flex justify-center items-center flex-1 ${!vertical && "transform -rotate-90 w-8"}`}>{title}</p>
         <div className="flex justify-center gap-2">
-          {buttons.map((button) => (
-            <button key={button.key} onClick={openModal(button.modal)} className="flex items-center text-2xl">
-              {button.icon}
-            </button>
-          ))}
+          {buttons &&
+            buttons.map((button) => (
+              <button key={button.key} onClick={openModal(button.modal)} className="flex items-center text-2xl">
+                {button.icon}
+              </button>
+            ))}
         </div>
       </div>
-      <div className="relative z-40">
-        {activeModal && (
-          <Modal closeModal={closeModal}>
-            <p className="text-xl text-center">{activeModal.title}</p>
-            {activeModal.component}
-          </Modal>
-        )}
-      </div>
+      {activeModal && (
+        <Modal closeModal={closeModal}>
+          <p className="text-xl text-center">{activeModal.title}</p>
+          {activeModal.component}
+        </Modal>
+      )}
     </>
   );
 };
