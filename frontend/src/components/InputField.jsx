2d4f23e165d24808f62ad556e65841387c54a869
Tiago <tiagowu12@gmail.com>
2023-08-03 15:59:58 -0400

    Add support for 'textarea' type in InputField component

--- frontend/src/components/InputField.jsx
+++ frontend/src/components/InputField.jsx
@@ -1,7 +1,7 @@
 import { forwardRef, useState } from "react";
 
 const InputField = forwardRef((props, ref) => {
-  const { id, type, label, maxLength, min, name, value, handleChange, handleKeyDown } = props;
+  const { id, type, label, maxLength, min, name, rows, value, handleChange, handleKeyDown } = props;
   const [showPass, setShowPass] = useState(false);
 
   const togglePasswordVisibility = () => {
@@ -14,21 +14,37 @@ const InputField = forwardRef((props, ref) => {
         {label}
       </label>
       <div className="relative flex justify-center">
-        <input
-          className="shadow border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:border-blue-400"
-          autoComplete="off"
-          id={id}
-          maxLength={maxLength}
-          min={min}
-          name={name}
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-          ref={ref}
-          spellCheck={false}
-          type={showPass ? "text" : type}
-          value={value}
-          required
-        />
+        {type === "textarea" ? (
+          <textarea
+            className="shadow border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:border-blue-400 resize-none scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-slate-300 scrollbar-track-transparent scrollbar-track-rounded"
+            id={id}
+            maxLength={maxLength}
+            name={name}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            ref={ref}
+            spellCheck={false}
+            value={value}
+            required
+            rows={rows}
+          />
+        ) : (
+          <input
+            className="shadow border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:border-blue-400"
+            autoComplete="off"
+            id={id}
+            maxLength={maxLength}
+            min={min}
+            name={name}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            ref={ref}
+            spellCheck={false}
+            type={showPass ? "text" : type}
+            value={value}
+            required
+          />
+        )}
         {type === "password" && value && (
           <button className="absolute right-2 top-2 text-gray-500 focus:outline-none" type="button" onClick={togglePasswordVisibility}>
             {showPass ? "Hide" : "Show"}
