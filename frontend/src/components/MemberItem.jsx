e946f4c2e1b0505bc631380394ea9d52feec8bbe
Tiago <tiagowu12@gmail.com>
2023-08-04 21:11:52 -0400

    Update buttons and handlers in MemberItem component

--- frontend/src/components/MemberItem.jsx
+++ frontend/src/components/MemberItem.jsx
@@ -1,28 +1,61 @@
-import { MdArrowDownward, MdArrowUpward, MdOutlinePersonRemove } from "react-icons/md";
+import { useParams } from "react-router-dom";
+import { MdArrowDownward, MdArrowUpward, MdPersonRemove } from "react-icons/md";
 
-const MemberItem = ({ item }) => {
+import { removeMember, updateMember } from "../api/team";
+import useAuth from "../hooks/useAuth";
+import useTeam from "../hooks/useTeam";
+
+const MemberItem = ({ item, role }) => {
+  const { auth } = useAuth();
+  const { teamId } = useParams();
+  const { setTeam } = useTeam();
+
+  const handleRemoveMember = async (memberId) => {
+    try {
+      const response = await removeMember(teamId, memberId, auth.accessToken);
+      setTeam((prevTeam) => ({
+        ...prevTeam,
+        members: response.members,
+      }));
+    } catch (err) {}
+  };
+
+  const handleUpdateMember = async (memberId, data) => {
+    try {
+      const response = await updateMember(data, teamId, memberId, auth.accessToken);
+      setTeam((prevTeam) => ({
+        ...prevTeam,
+        members: response.members,
+      }));
+    } catch (err) {}
+  };
+
   return (
     <div className="w-full bg-inherit px-2">
       <div className="flex flex-row justify-between w-full bg-slate-300 rounded-lg p-2">
-        <div className="flex flex-col overflow-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-rounded scrollbar-thumb-gray-400/20 scrollbar-track-transparent scrollbar-track-rounded">
+        <div className="flex flex-col whitespace-nowrap overflow-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-rounded scrollbar-thumb-gray-400/20 scrollbar-track-transparent scrollbar-track-rounded">
           <p className="text-lg">{item.name}</p>
           <p className="text-gray-600 text-sm"> {item.role}</p>
         </div>
         <div className="flex flex-row gap-2 text-2xl">
-          {item.role === "Member" && (
-            <button className="">
-              <MdArrowUpward />
-            </button>
-          )}
-          {item.role === "Co-Manager" && (
-            <button className="">
-              <MdArrowDownward />
-            </button>
-          )}
-          {item.role !== "Manager" && (
-            <button className="">
-              <MdOutlinePersonRemove />
-            </button>
+          {role === "Manager" && (
+            <>
+              {item.role === "Member" && (
+                <button onClick={() => handleUpdateMember(item._id, { role: "Co-Manager" })}>
+                  <MdArrowUpward />
+                </button>
+              )}
+              {item.role === "Co-Manager" && (
+                <button onClick={() => handleUpdateMember(item._id, { role: "Member" })}>
+                  <MdArrowDownward />
+                </button>
+              )}
+              {item.role !== "Manager" && (
+                <button onClick={() => handleRemoveMember(item._id)}>
+                  <MdPersonRemove />
+                </button>
+              )}
+            </>
           )}
         </div>
       </div>
