a08e0e8b34b4388013834eb8608a823003343946
Tiago <tiagowu12@gmail.com>
2023-08-07 15:23:27 -0400

    Update index.js to render the App component within TeamProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,14 +5,17 @@ import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { LoadingProvider } from "./context/LoadingContext";
 import { MessageProvider } from "./context/MessageContext";
+import { TeamProvider } from "./context/TeamContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthProvider>
-    <LoadingProvider>
-      <MessageProvider>
-        <App />
-      </MessageProvider>
-    </LoadingProvider>
+    <TeamProvider>
+      <LoadingProvider>
+        <MessageProvider>
+          <App />
+        </MessageProvider>
+      </LoadingProvider>
+    </TeamProvider>
   </AuthProvider>
 );
